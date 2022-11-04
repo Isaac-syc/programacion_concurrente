b394f829292b1c64dbb69ebae2ac6b12439cb823
Isaac-syc <kevinvelascor@hotmail.com>
2022-11-04 13:19:29 -0600

    fixes

--- restaurante/Receptionist.py
+++ restaurante/Receptionist.py
@@ -20,14 +20,19 @@ class Receptionist(Thread):
     
     def giveTable(self, name, is_reserved, max_customers, reservations, enqueue, queueTables, tables):
         self.condition.acquire()
+        in_table = False
         if len(tables)  < max_customers and is_reserved:
             tables.append(name)
             print(f"-El cliente {name} esta en SU MESA POR RESERVACION")
+            in_table = True
         elif len(tables) < max_customers-2:
             tables.append(name)
             print(f"-El cliente {name} esta en SU MESA")
+            in_table = True
         else:
             queueTables.put(name)
             print(f"-El cliente {name} esta en COLA DE MESAS")
+            in_table = False
         self.condition.notify()
         self.condition.release()
+        return in_table
